e98f2309268bba9ed1c865b1b1f5b80ed128209a
Vicente Juarez <vjuarez_@outlook.com>
2025-03-17 14:16:55 -0600

    Creando func sorteo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Los nombres de dos funciones ya están dados por el HTML proporcionado, no se cambian para no confundir al equipo.
+
+let amigos = [];
+function agregarAmigo (){
+    let nombreAmigo = document.getElementById("amigo");
+
+}
+
+let sortearAmigo(){
+    if (amigos.length === 0){
+        alert('No has añadido ningún amigo a la lista. Añade amigos para sortear');
+        return;
+    }
+    let sorteoDeAmigos = Math.floor(Math.random() * amigos.length);
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Tu amigo secreto es: ${sorteoDeAmigos}`;    
+}
